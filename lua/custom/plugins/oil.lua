b3f2401778726126f96d4a7cc0a3a6bc6253b7ff
braedonm <braedonm@kineticsdrive.com>
2024-07-23 10:10:29 -0700

    Oil now toggles detailed view with

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,6 +1,7 @@
 return {
   'stevearc/oil.nvim',
   config = function()
+    local detail = false
     require('oil').setup {
       keymaps = {
         ['g?'] = 'actions.show_help',
@@ -19,6 +20,17 @@ return {
         ['gx'] = 'actions.open_external',
         ['g.'] = 'actions.toggle_hidden',
         ['g\\'] = 'actions.toggle_trash',
+        ['gh'] = {
+          desc = 'Toggle file detail view',
+          callback = function()
+            detail = not detail
+            if detail then
+              require('oil').set_columns { 'icon', 'permissions', 'size', 'mtime' }
+            else
+              require('oil').set_columns { 'icon' }
+            end
+          end,
+        },
       },
       use_default_keymaps = false,
       watch_for_changes = true,
@@ -27,7 +39,6 @@ return {
         show_hidden = true,
       },
       columns = {
-        -- 'size',
         'icon',
       },
     }
