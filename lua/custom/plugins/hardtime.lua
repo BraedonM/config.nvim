eeeee68b4672d5342f10f50360df5d9aa8f6a9d9
braedonm <braedonm@kineticsdrive.com>
2024-08-02 15:00:13 -0700

    Removed hardtime; stupid plugin

--- lua/custom/plugins/hardtime.lua
+++
@@ -1,9 +0,0 @@
-return { -- Shows when commands could be more efficient
-  'm4xshen/hardtime.nvim',
-  dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
-  opts = {
-    max_time = 1,
-    -- disable_mouse = false,
-    -- restriction_mode = 'hint',
-  },
-}
